8d2636c4049862023bc88e987b8f32dc5938756b
unknownguytoday@gmail.com <34234631+rmiaas@users.noreply.github.com>
2019-04-25 19:29:55 +0800

    Remove react router transition messages and use hash based urls instead of history locations

--- src/components/about/aboutPage.js
+++ src/components/about/aboutPage.js
@@ -3,20 +3,6 @@
 var React = require('react');
 
 var About = React.createClass({
-	statics: {
-		willTransitionTo: function (transition, params, query, callback) {
-			if (!confirm("Are you sure you want to read a page that\'s this boring?")) {
-				transition.abort();
-			} else {
-				callback();
-			}
-		},
-		willTransitionFrom: function (transition, component) {
-			if (!confirm("Are you sure you want to read a leave that\'s this exciting?")) {
-				transition.abort();
-			}
-		}
-	},
 	render: function () {
 		return (
 			<div>
